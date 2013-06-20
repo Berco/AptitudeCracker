d07c40bceb687a2ecbedd2a690164d7c9c9b83f2
Berco Bosker <berco.bosker@gmail.com>
2013-06-21 01:26:35 +0200

    WIP  - shared pref highscores

--- src/com/droidacid/apticalc/tys/TYSScores.java
+++ src/com/droidacid/apticalc/tys/TYSScores.java
@@ -1,18 +1,24 @@
 package com.droidacid.apticalc.tys;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import com.droidacid.apticalc.R;
-import com.droidacid.apticalc.aptitudes.AptiFactorial;
+import com.droidacid.apticalc.tys.model.ScoreEntry;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class TYSScores extends Activity implements OnClickListener {
 	
@@ -48,12 +54,77 @@ public class TYSScores extends Activity implements OnClickListener {
 		wrongAns = getScore.getInt("wronganswers");
 		score = getScore.getInt("score");
 		
+		if (isHighScore(score)) saveScore(score);
+		
 		getValuesToString();
 		
 		setValuesToTextViews();
 	}
 
 	
+
+	private void saveScore(int score) {
+		Toast.makeText(this, "HighScore!!!", Toast.LENGTH_LONG).show();
+		
+		SharedPreferences getPrefs = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+		int numberEntries = getPrefs.getInt("entries", 0);
+		ScoreEntry newEntry = new ScoreEntry("shivam", score);
+		List<ScoreEntry> entries = new ArrayList<ScoreEntry>(numberEntries);
+		for (int i = 1;  i <= numberEntries ; i++){
+			String high = getPrefs.getString("high_"+Integer.toString(i), "name,12");
+			String[] highA = high.split(",");
+			ScoreEntry entry = new ScoreEntry(highA[0], Integer.valueOf(highA[1]));
+			entries.add(entry);
+		}
+		Collections.sort(entries);
+		if (entries.size() < 5){
+			entries.add(newEntry);
+		} else {
+			entries.remove(entries.size()-1);
+			entries.add(newEntry);
+		}
+		Collections.sort(entries);
+		
+		Editor editor = getPrefs.edit();
+		for (int i = 1; i < entries.size(); i++){
+			String aName= entries.get(i).getName();
+			String aScore = Integer.toString(entries.get(i).getScore());
+			editor.putString("high_"+Integer.toString(i), aName+","+aScore);
+		}
+		editor.putInt("entries", entries.size());
+        editor.commit();
+	}
+
+	private boolean isHighScore(int score) {
+		SharedPreferences getPrefs = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+		int numberEntries = getPrefs.getInt("entries", 0);
+		Log.d(tag, "entries = " + Integer.toString(numberEntries));
+		// only check if there are entries to be compared with
+		if (numberEntries > 0){
+			// if we have less than 6 entries, also a new high score
+			if (numberEntries < 6) return true;
+			
+			List<ScoreEntry> entries = new ArrayList<ScoreEntry>(numberEntries);
+			for (int i = 1;  i <= numberEntries ; i++){
+				String high = getPrefs.getString("high_"+Integer.toString(i), "name,12");
+				String[] highA = high.split(",");
+				ScoreEntry entry = new ScoreEntry(highA[0], Integer.valueOf(highA[1]));
+				entries.add(entry);
+			}
+			Collections.sort(entries);
+			// higher than the lowest one known
+			if ((score >= entries.get(entries.size()-1).getScore()) ){
+				int oldHighest = entries.get(entries.size()-1).getScore();
+				Log.d(tag, "new HighScore: "+ Integer.toString(score)+ " ,higher than " + Integer.toString(oldHighest));
+				return true;
+			}
+			// this score was not higher than the lowest one known
+			return false;
+		} else {
+			// this score was the first score ever! 
+			return true;
+		}
+	}
 
 	private void getValuesToString() {
 		Log.d(tag, "inside getValuesToString method");
@@ -81,7 +152,6 @@ public class TYSScores extends Activity implements OnClickListener {
 			startActivity(highScore);
 		break;
 		case R.id.b_tys_PlayAgain:
-			
 			Intent playAgain = new Intent(this, OneDifficulty.class);
 			startActivity(playAgain);
 		break;
